69f688e60a95ae2301bfa93a725e3db67e6bb56e
linetlucy genchabe <linetlucy21@gmail.com>
2022-04-09 12:44:33 +0300

    update date and votes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,8 +1,11 @@
 
 export class Quotes {
     showDescription: boolean = false;
+  isFavorite!: boolean;
+  upvotes!: number;
+  downvotes!: number;
 
-    constructor(public id: number,public name: string,public description: string,public startDate: Date){
+    constructor(public id: number,public name: string,public description: string,public startDate: Date, upvotes:number,downvotes:number,){
      
     }
 }
