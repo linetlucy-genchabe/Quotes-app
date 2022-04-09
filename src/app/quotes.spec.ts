fcae7d75110c02db424cf76f90788b702919a42f
linetlucy genchabe <linetlucy21@gmail.com>
2022-04-09 12:37:42 +0300

    add new quotes parameters

--- src/app/quotes.spec.ts
+++ src/app/quotes.spec.ts
@@ -2,6 +2,6 @@ import { Quotes } from './quotes';
 
 describe('Quotes', () => {
   it('should create an instance', () => {
-    expect(new Quotes()).toBeTruthy();
+    expect(new Quotes(0,"","",new Date(),0,0)).toBeTruthy();
   });
 });
